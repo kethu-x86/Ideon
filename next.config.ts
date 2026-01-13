135c3690f05c9d55b769120b68259818e522a7c4
kethu-x86 <anukhethsunil@gmail.com>
2026-01-13 22:15:57 +0530

    Add cloudflare stuff

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  images: {
-    unoptimized: false,
-  },
 };
 
 export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
